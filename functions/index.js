ed3e36cda26cbaed17d793f432f64e3856f52413
D-Ronald <danielronald21@gmail.com>
2026-09-13 14:12:34 -0300

    Add Firebase project config and remove scheduled Cloud Functions
    Scheduled jobs (keep-alive, ETo horária, pipeline diário) foram migrados
    para GitHub Actions (.github/workflows/scheduled-jobs.yml).

---
+++ functions/index.js
@@ -0,0 +1,5 @@
+/**
+ * Os jobs agendados (keep-alive, ETo horária e pipeline diário) foram
+ * migrados para GitHub Actions (.github/workflows/scheduled-jobs.yml)
+ * para evitar o plano Blaze do Firebase (Cloud Scheduler/Functions v2).
+ */
